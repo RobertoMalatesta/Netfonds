9cb5bc6859a47a532749d2b35f47718ee5187399
Michael Park <migrap@gmail.com>
2013-12-26 15:14:20 -0800

    Moved stuff around

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,23 @@
+﻿using Netfonds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox {
+    class Program {
+        static void Main(string[] args) {
+            var client = new NetfondsClient();
+            var trades = client.GetTradesAsync(x => x
+                .Date(DateTimeOffset.Now.AddDays(-1))
+                .Exchange("O")
+                .Symbol("AAPL")
+            ).Result;
+
+            foreach(var trade in trades) {
+                Console.WriteLine(trade);
+            }
+        }
+    }
+}
